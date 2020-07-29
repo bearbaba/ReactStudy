394e676fecf9300a15ef28947172aad09a4201c6
bearbaba <2200522850@qq.com>
2020-07-29 21:20:36 +0800

    这里面有问题亟待解决

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,31 +2,71 @@ import React from 'react';
 import Note from './components/Note';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
-const App = () => {
+const App = (props) => {
   
   
-  const [state, setState] = useState([]);
+  const [notes, setNotes] = useState([]);
 
+  setNotes(props);
 
+  const [newNote, setNewNote] = useState("Hello");
 
-  useEffect(()=>{
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) =>{
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
+    }
+
     axios
-      .get("http://localhost:3001/notes")
-      .then((response)=>{
-        console.log('promise fulfilled');
-        setState(response.data);
+      .post("http://localhost:3001/notes", noteObject)
+      .then(response=>{
+        setNotes(notes.concat(response.data));
+        setNewNote('');
       })
-  }, [])
-  
-  console.log('render', state.length, 'notes');
+  }
+
+  const handleInputChange = (event)=>{
+    return (
+      setNewNote(event.target.value)
+    )
+  }
+
+  const notesToShow = notes.filter(note => note.important);
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios.put(url, changedNote).then(response=>{
+      setNotes(notes.map(note=>note.id !== id ? note : response.data))
+    })
+  }
+
+
+  console.log(notesToShow);
+  // console.log('render',notes.length, 'notes');
 
   return (
     <div>
+      <h1>Notes</h1>
+      <button onClick={setShowAll(!showAll)}>
+        show {showAll ? "all" : "important"}
+      </button>
       <ul>
         {
-          state.map((note)=> {return (<Note key={note.id} note={note}/>)})
+          notesToShow.map(note => <Note key={note.id} note={note} />)
         }
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleInputChange}/>
+        <button type="submit" >submit</button>
+      </form>
     </div>
   )
 }
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
+import {useEffect} from 'react';
 import App from './App';
 
-axios
+useEffect(() => {
+  axios
+    .get("http://localhost:3001/notes")
+    .then((response) => {
+      console.log('promise fulfilled');
+      const props = response.data;
+      ReactDOM.render(<App props={props}/>, document.getElementById('root'))
+    })
+}, [])
+/* axios
   .get("http://localhost:3001/notes")
   .then((response)=>{
     // console.log(response.data);
@@ -13,3 +22,5 @@ axios
     ReactDOM.render( < App />, document.getElementById('root'));
     //这是个异步方法，会首先执行同步再执行异步，渲染组件的方法是同步的，所以写进了这里
   })
+ */
+// ReactDOM.render(<App/>, document.getElementById('root'));
